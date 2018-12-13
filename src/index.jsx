3bcf3457b2d52b7f16f163cb81f654548126a6ed
Andrew Rogala <adrogala87@gmail.com>
2018-12-13 12:19:27 -0500

    save state to local storage

--- src/index.jsx
+++ src/index.jsx
@@ -5,10 +5,25 @@ import { createStore } from 'redux';
 
 import reducers from './redux/reducers';
 import App from './App';
+import { loadState, saveState } from './localStorage';
 import * as serviceWorker from './serviceWorker';
 
+const persistedState = loadState();
+console.log(persistedState);
+const store = createStore(
+	reducers,
+	persistedState
+);
+
+store.subscribe(() => {
+	console.log(store.getState());
+	saveState({
+		toDoListItems: store.getState().toDoListItems
+	});
+});
+
 ReactDOM.render(
-	<Provider store={createStore(reducers)}>
+	<Provider store={store}>
 		<App />
 	</Provider>,
 	document.getElementById('root')
